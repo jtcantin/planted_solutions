d12f40a2b50a6a07baac25847c63f5b321b5bff7
Rick0317 <rick.hey.huang@gmail.com>
2024-04-16 14:48:47 -0400

    Refactored some files

--- DMRG_simulation/utils/fci_ground_energy.py
+++ DMRG_simulation/utils/fci_ground_energy.py
@@ -1,6 +1,7 @@
 import openfermion as of
 import CAS.ferm_utils as feru
 
+
 def get_fci_ground_energy(tbt):
     recombined = feru.get_ferm_op(tbt, True)
     sparse = of.linalg.get_sparse_operator(recombined)
